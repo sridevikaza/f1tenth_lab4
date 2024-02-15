e63f37834ddee13c968c3fb489b0126407d9e86e
sridevikaza <39656682+sridevikaza@users.noreply.github.com>
2024-02-15 20:31:37 +0000

    working!

--- gap_follow/src/reactive_node.cpp
+++ gap_follow/src/reactive_node.cpp
@@ -27,13 +27,12 @@ public:
 private:
     string lidarscan_topic = "/scan";
     string drive_topic = "/drive";
-    double rejection_thresh = 5.0;
+    double rejection_thresh = 10.0;
     double filter_window = 5.0;
-    int bubble_radius = 10;
-    double velocity = 0.5;
-    double disparity_thresh = 0.4;
-    double disparity_window = 10.0;
-    double gap_thresh = 0.7;
+    int bubble_radius = 5;
+    double disparity_thresh = 0.1;
+    double disparity_window = 5.0;
+    double gap_thresh = 2.0;
 
     /// ROS subscribers and publishers
     rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_publisher_;
@@ -47,23 +46,23 @@ private:
         // const int ranges_size = sizeof(ranges) / sizeof(ranges[0]);
 
         float closest_val = numeric_limits<float>::max();
-        int closest_idx;
+        int closest_idx = 0;
 
         float* smoothed_ranges = new float[ranges_size];
 
         for (int i=0; i<ranges_size; i++){
-            RCLCPP_INFO(this->get_logger(), "range: % f", ranges[i]);
+
             // set large values to rejection thresh
             if(ranges[i] > rejection_thresh){
                 smoothed_ranges[i] = rejection_thresh;
             }
 
-            // handle nan values -- set to 0
+            // set nan values to 0
             else if (isnan(ranges[i])){
-                smoothed_ranges[i] = 0.0;
+                smoothed_ranges[i] = rejection_thresh;
             }
 
-            // moving average filter to smooth ranges
+            // moving average filter
             else{
                 double sum = 0;
                 double total = 0;
@@ -82,20 +81,19 @@ private:
                     }
                 }
                 smoothed_ranges[i] = sum / total;
-                RCLCPP_INFO(this->get_logger(), "smoothed_range: % f", smoothed_ranges[i]);
 
                 // keep track of closest point
-                if (smoothed_ranges[i] < closest_val){
+                if (smoothed_ranges[i] <= closest_val){
                     closest_val = smoothed_ranges[i];
                     closest_idx = i;
                 }
+
             }
         }
 
         // RCLCPP_INFO(this->get_logger(), "closest_idx: %i", closest_idx);
         return make_pair(closest_idx, smoothed_ranges);
     }
-
 
     void find_max_gap(float* ranges, int* indices, const int ranges_size)
     {
@@ -105,7 +103,7 @@ private:
         int max_gap_size = 0;
 
         while(end < ranges_size){
-            if (ranges[end]==0 || end==ranges_size-1){
+            if (ranges[end]<gap_thresh || end==ranges_size-1){
                 if (end-start > max_gap_size){
                     max_gap_size = end-start;
                     indices[0] = start;
@@ -116,45 +114,6 @@ private:
             }
             end++;
         }
-    }
-
-    void find_deep_gap(float* ranges, int* indices, const int ranges_size)
-    {
-        // Return the start index & end index of the max gap in free_space_ranges
-        int start = indices[0];
-        int end = indices[1];
-        int max_gap_size = 0;
-
-        while(end < ranges_size){
-            if (ranges[end]>gap_thresh || end==ranges_size-1){
-                if (end-start > max_gap_size){
-                    max_gap_size = end-start;
-                    indices[0] = start;
-                    indices[1] = end-1;
-                }
-                end++;
-                start = end;
-            }
-            end++;
-        }
-    }
-
-
-    int find_best_point(float* ranges, int* indices)
-    {   
-        // Start_i & end_i are start and end indicies of max-gap range, respectively
-        // Return index of best point in ranges
-	    // Naive: Choose the furthest point within ranges and go there
-        double best_point = 0;
-        int best_idx = 0;
-
-        for (int i=indices[0]; i<indices[1]; i++){
-            if (ranges[i] > best_point){
-                best_point = ranges[i];
-                best_idx = i;
-            }
-        }
-        return best_idx;
     }
 
     void disparity_extender(float* ranges, const int ranges_size){
@@ -192,41 +151,51 @@ private:
         }
     }
 
+    double calc_velocity(double angle){
+        if (angle < 10*M_PI/180){ // 0 to 10 degrees
+            return 5.0; //2.0
+            // return 2.0;
+        }
+        else if (angle < 20*M_PI/180){ // 10 to 20 degrees
+            return 4.0; //1.0
+            // return 1.0;
+        }
+        else{ // > 20 degrees
+            return 3.0; //0.5
+            // return 0.5;
+        }
+    }
+
 
     void lidar_callback(const sensor_msgs::msg::LaserScan::ConstSharedPtr scan_msg) 
     {
         // Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
-
-        // preprocess lidar range data 
+        // preprocess lidar range data
         auto preprocess = preprocess_lidar(scan_msg->ranges.data(), scan_msg->ranges.size());
 
         // Find closest point to LiDAR
         auto closest_idx = preprocess.first;
         auto ranges = preprocess.second;
-        disparity_extender(ranges, scan_msg->ranges.size());
+        // disparity_extender(ranges, scan_msg->ranges.size());
 
         // Eliminate all points inside 'bubble' (set them to zero)
-        for (int i = max(closest_idx-bubble_radius, 0); i < min(closest_idx+bubble_radius, static_cast<int>(scan_msg->ranges.size())); ++i) {
+        for (int i = closest_idx-bubble_radius; i < closest_idx+bubble_radius; i++) {
+            if( i < 0 || i > (int)scan_msg->ranges.size()) continue;
             ranges[i] = 0;
         }
 
         // Find max length gap
         int indices[] = {0,1};
         find_max_gap(ranges, indices, scan_msg->ranges.size());
-        // find_deep_gap(ranges, indices, scan_msg->ranges.size());
-        RCLCPP_INFO(this->get_logger(), "max gap size: %d", indices[1]-indices[0]);
 
         // Find the best point in the gap
-        int best_point = find_best_point(ranges, indices); // for furthest point
-        // int best_point = (indices[0]+indices[1])/2; // for midpoint
-        RCLCPP_INFO(this->get_logger(), "best point index: %d", best_point);
+        int best_point = (indices[0]+indices[1])/2; // for midpoint
 
         // Publish Drive message
         double steering_angle = scan_msg->angle_min + best_point*scan_msg->angle_increment;
         auto drive_msg = ackermann_msgs::msg::AckermannDriveStamped();
-        drive_msg.drive.speed = velocity;
+        drive_msg.drive.speed = calc_velocity(steering_angle);
         drive_msg.drive.steering_angle = steering_angle;
-        RCLCPP_INFO(this->get_logger(), "steering angle: %f", steering_angle);
         drive_publisher_->publish(drive_msg);
 
         delete[] ranges;
